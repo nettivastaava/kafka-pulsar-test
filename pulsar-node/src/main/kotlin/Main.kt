4d99f30413ae4f6f7c3a2d3ef347c7f7895bf2c9
Valtteri Andström <valtteri.andstrom@elisa.fi>
2023-12-08 12:15:35 +0200

    improved workflow

--- pulsar-node/src/main/kotlin/Main.kt
+++ pulsar-node/src/main/kotlin/Main.kt
@@ -6,20 +6,26 @@ import utils.ApplicationConfigService
 fun main() {
     val pulsarUrl = "pulsar://pulsar:6650"
 
-    val client = PulsarClient.builder()
-        .serviceUrl(pulsarUrl)
-        .build()
+    while (true) {
+        try {
+            val client = PulsarClient.builder()
+                .serviceUrl(pulsarUrl)
+                .build()
 
-    val applicationConfig = ApplicationConfigService.getApplicationConfig()
-    val type = applicationConfig.type
+            val applicationConfig = ApplicationConfigService.getApplicationConfig()
+            val type = applicationConfig.type
 
-    if (type == "SOURCE") {
-        val pulsarSource = PulsarSource(client = client)
+            if (type == "SOURCE") {
+                val pulsarSource = PulsarSource(client = client)
 
-        pulsarSource.start()
-    } else {
-        val pulsarDestination = PulsarDestination(client = client)
+                pulsarSource.start()
+            } else {
+                val pulsarDestination = PulsarDestination(client = client)
 
-        pulsarDestination.start()
+                pulsarDestination.start()
+            }
+        } catch (ex: Exception) {
+            println("Failed due to $ex")
+        }
     }
 }
